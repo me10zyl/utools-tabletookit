25ea88c3429916d4c1235b0ca967baa47e1e45d5
zyl <me10zyl@qq.com>
2024-04-24 15:30:22 +0800

    tempCommit

---
+++ src/components/TableQuickQuery.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'TableQuickQuery'
+}
+</script>
+
+<template>
+<div>
+
+  <v-card>
+    <template v-slot:title>
+      <span class="font-weight-black">快速查询顾客</span>
+    </template>
+    <v-card-text>
+      select * from eq_mall_info where mall_id = 9014 limit 10
+    </v-card-text>
+  </v-card>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Mainboard.vue
+++ src/views/Mainboard.vue
@@ -1,10 +1,11 @@
 <script>
 import Table from "@/components/Table.vue";
 import TableConf from "@/components/TableConf.vue";
+import TableQuickQuery from "@/components/TableQuickQuery.vue";
 
 export default {
   name: 'MainBoard',
-  components: {TableConf, Table},
+  components: {TableConf, TableQuickQuery, Table},
   data() {
     return {
       tab: null
@@ -30,6 +31,13 @@ export default {
           @update:model-value="changeTab"
           v-model="tab">
         <v-tab value="table">表结构查询</v-tab>
+        <v-tab value="tableQuickQuery">
+          <v-tooltip text="构建快速查询SQL：如根据某个表的条件快速查询某个表的数据">
+            <template v-slot:activator="{ props }">
+              <span v-bind="props">预查询</span>
+            </template>
+          </v-tooltip>
+        </v-tab>
         <v-tab value="tableConf">数据库配置</v-tab>
       </v-tabs>
 
@@ -40,6 +48,9 @@ export default {
           </v-window-item>
           <v-window-item value="tableConf">
             <TableConf ref="tableConf"/>
+          </v-window-item>
+          <v-window-item value="tableQuickQuery">
+            <TableQuickQuery ref="tableQuickQuery"/>
           </v-window-item>
 
         </v-window>
